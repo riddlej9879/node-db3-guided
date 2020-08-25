13890418e795b95356f9f9dce6959d1772f5fb74
Josh Riddle <riddlej9879@yahoo.com>
2020-08-24 20:42:25 -0500

    Guided project complete

---
+++ users/user-model.js
@@ -0,0 +1,16 @@
+const db = require("../data/config");
+
+function findPostsByUserID(userID) {
+  return (
+    db("posts as p")
+      // Grabs Users table.
+      // users "u.id" is compared to posts "p.user_id"
+      .innerJoin("users as u", "u.id", "p.user_id")
+      .where("p.user_id", userID)
+      .select("p.id", "p.contents", "u.username")
+  );
+}
+
+module.exports = {
+  findPostsByUserID,
+};
--- users/user-router.js
+++ users/user-router.js
@@ -1,76 +1,68 @@
-const express = require("express")
-const db = require("../data/config")
+const express = require("express");
+const db = require("../data/config");
+const userModel = require("./user-model");
+const { validateUserId } = require("./user-middleware");
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/users", async (req, res, next) => {
-	try {
-		res.json(await db("users"))
-	} catch(err) {
-		next(err)
-	}
-})
+  try {
+    res.json(await db("users"));
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.get("/users/:id", validateUserId(), async (req, res, next) => {
-	try {
-		res.json(req.user)
-	} catch(err) {
-		next(err)
-	}
-})
+  try {
+    res.json(req.user);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.post("/users", async (req, res, next) => {
-	try {
-		const [id] = await db("users").insert(req.body)
-		const user = await db("users").where({ id }).first()
+  try {
+    const [id] = await db("users").insert(req.body);
+    const user = await db("users").where({ id }).first();
 
-		res.status(201).json(user)
-	} catch(err) {
-		next(err)
-	}
-})
+    res.status(201).json(user);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.put("/users/:id", validateUserId(), async (req, res, next) => {
-	try {
-		const { id } = req.params
-		await db("users").where({ id }).update(req.body)
-		const user = await db("users").where({ id }).first()
-		
-		res.json(user)
-	} catch(err) {
-		next(err)
-	}
-})
+  try {
+    const { id } = req.params;
+    await db("users").where({ id }).update(req.body);
+    const user = await db("users").where({ id }).first();
+
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.delete("/users/:id", validateUserId(), async (req, res, next) => {
-	try {
-		const { id } = req.params
-		await db("users").where({ id }).del()
+  try {
+    const { id } = req.params;
+    await db("users").where({ id }).del();
 
-		res.status(204).end()
-	} catch(err) {
-		next(err)
-	}
-})
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
 
-function validateUserId() {
-	return async (req, res, next) => {
-		try {
-			const { id } = req.params
-			const user = await db("users").where({ id }).first()
+// create an endpoint for listing a user's posts
+router.get("/users/:id/posts", validateUserId(), async (req, res, next) => {
+  try {
+    const posts = await userModel.findPostsByUserID(req.params.id);
+    res.json(posts);
+  } catch (err) {
+    next(err);
+  }
+});
 
-			if (!user) {
-				return res.status(404).json({
-					message: "User not found",
-				})
-			}
-
-			req.user = user
-			next()
-		} catch(err) {
-			next(err)
-		}
-	}
-}
-
-module.exports = router
+module.exports = router;
